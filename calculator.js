43111accdef2418be3e7da6491c8cc84b79c0c02
Abdul Hafeez <hafizabdulhafeez240302@gmail.com>
2023-07-16 15:25:00 +0530

    processing post request with body parser

--- calculator.js
+++ calculator.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const bodyParser = require("body-parser");      //for fetching the info from post
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+//body-parser is used with express
+//bodyParser.text will give all info in txt formate
+//bodyparser.json will give us in js object fromate
+//bodyParser.urlencoded 
 
 app.listen(3000, function() {
     console.log(`Server started at port 3000.`);
@@ -10,4 +18,15 @@ app.get("/", function (req, res) {
     // console.log(__dirname);
     res.sendFile(__dirname+"/index.html");
     //__dirname will return the path of dir from root
+});
+
+//now we are proceccing the post request
+app.post("/", function (req, res) {
+    // console.log(req.body);
+    // console.log(req.body.num1);
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result =num1+num2;
+
+   res.send("The Result is "+result); 
 });
